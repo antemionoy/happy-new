9b47b8871128c125a4d38622ebced47670c5211b
Anton <antemionoy@gmail.com>
2022-02-19 18:38:43 +0300

    create project

---
+++ src/components/populars/index.js
@@ -0,0 +1,19 @@
+const Populars = () =>{
+    return(
+        <section className="popular">
+            <div className="popular__container container">
+                <h2 className="popular__title title title_h2">Популярные кухни</h2>
+                <ul className="popular__row d-flex wrap">
+                    <li className="popular__column">
+                        <div className="popular__item"></div>
+                    </li>
+                    <li className="popular__column">
+                        <div className="popular__item"></div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Populars;
